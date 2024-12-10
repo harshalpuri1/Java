d1577de388bfbc82283d0d4d7ef038109c4a4140
Harshal Puri <hrp3102002@gmail.com>
2024-12-10 15:10:57 +0530

    Subarrays

--- Arrays/Subarrays.java
+++ Arrays/Subarrays.java
@@ -1,26 +1,4 @@
 public class Subarrays {
-
-    public static void PrintMaxSubarrays(int array[]){
-        int curr = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < array.length; i++) {
-            int start = i;
-            for(int j=i; j< array.length; j++){
-                int end = j;
-                curr = 0;
-                for(int k=start; k<=end; k++){
-                    curr += array[k];
-                    if (max < curr) {
-                        System.out.println("Current sum: "+curr);
-                        max = curr;
-                    }
-                }
-            }
-        }
-        System.out.println("Max Sum: "+max);
-    }
-
-
     public static void PrintSubarrays(int array[]){
         int ttl = 0;
         for (int i = 0; i < array.length; i++) {
@@ -36,8 +14,54 @@ public class Subarrays {
             }System.out.println();
         }System.out.println(ttl);
     }
+
+    public static void PrintMaxSubarrays(int array[]){
+        int curr = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            int start = i;
+            for(int j=i; j< array.length; j++){
+                int end = j;
+                curr = 0;
+                for(int k=start; k<=end; k++){
+                    curr += array[k];
+                }
+                if (max < curr) {
+                    System.out.println("Current sum: "+curr);
+                    max = curr;
+                }
+            }
+        }
+        System.out.println("Max Sum: "+max);
+    }
+    public static void PrifixMaxSubarrays(int array[]){
+        int curr = 0;
+        int max = Integer.MIN_VALUE;
+        int prefix[] = new int[array.length];
+        prefix[0]= array[0];
+        for(int i=1; i<array.length; i++){
+            prefix[i] = prefix[i-1] + array[i];
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            int start = i;
+            for(int j=i; j< array.length; j++){
+                int end = j;
+                curr = start==0 ? prefix[end] : prefix[end] - prefix[start-1];
+                
+                if (max < curr) {
+                    System.out.println("Current sum: "+curr);
+                    max = curr;
+                }
+            }
+        }
+        System.out.println("Max Sum: "+max);
+    }
+
+
     public static void main(String[] args) {
         int array[] ={10,20,2,4,6,8};
-        PrintMaxSubarrays(array);
+        // PrintMaxSubarrays(array);
+        PrifixMaxSubarrays(array);
     }
 }
