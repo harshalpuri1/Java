a3c6f3ed51fd71575a7deffbf55cf90baea06864
Harshal Puri <hrp3102002@gmail.com>
2024-12-17 14:05:26 +0530

    is Odd/Even

--- BIt Manipulation/OddEven.java
+++ BIt Manipulation/OddEven.java
@@ -7,29 +7,9 @@ public class OddEven {
         }
     }
 
-    public static int getithBit(int n, int i){
-        if ((n&(1<<i))==0) {
-            return 0;
-        }else{
-            return 1;
-        }
-    }
-
-    public static int setithBit(int n, int i){
-        int bitmask = 1<<i;
-        return n | bitmask;
-    }
-
-    public static int clearIthBit(int n, int i){
-        int bitmask = ~(1<<i);
-        return n & bitmask;
-    }
     public static void main(String[] args) {
         isOddEven(10);
         isOddEven(11);
         isOddEven(2);
-        System.out.println(getithBit(10, 3));
-        System.out.println(setithBit(10, 2));
-        System.out.println(clearIthBit(10, 2));
     }
 }
