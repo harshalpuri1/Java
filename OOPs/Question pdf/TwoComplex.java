6ed0c3dcd1c610c88d6e595665c6a8e21e45ee73
Harshal Puri <hrp3102002@gmail.com>
2025-02-25 14:13:32 +0530

    OOPs

---
+++ OOPs/Question pdf/TwoComplex.java
@@ -0,0 +1,17 @@
+public class TwoComplex {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Complex{
+    int n1;
+    int n2;
+
+    public Complex(int a , int b){
+        n1 = a;
+        n2 = b;
+    }
+
+    
+}
